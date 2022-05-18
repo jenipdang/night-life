ea60b15cd15ad73f7b79c6ac00776e1882ca6e26
Jeni Dang <jenipdang@yahoo.com>
2022-05-17 21:01:25 -0700

    finish up controllers:

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,55 @@
 class Api::CommentsController < ApplicationController
+    before_action :find_comment, only: [:show, :update, :destroy]
+
+    #GET "/comments" or GET "events/:event_id/comments"
+    def index 
+        if params[:event_id]
+            event = Event.find(params[:event_id])
+            render json: event.comments
+        else #GET "/comments"
+            render json: Comment.all
+        end
+    end
+
+    def show #get "/comments/:id"
+        render json: serialized_comment
+    end
+
+    def create #post "/comments" #post "posts/:post_id/comments"
+        if params[:post_id] 
+            post = Post.find(params[:post_id])
+            @comment = post.comments.create!(comment_params)
+            render json: serialized_comment, status: 201
+        end
+    end
+
+    def update #patch "/posts/:id"
+        if @comment&.update(comment_params) 
+            render json: serialized_comment
+        else
+            render json: {error: @comment.errors.full_messages.to_sentence}
+        end
+    end
+
+    def destroy #delete "/posts/:id"
+        if @comment&.destroy
+            render json: {message: "Successfully destroyed comment!"}
+        else
+            render json: {error: @comment.errors.full_messages.to_sentence}
+        end
+    end
+
+    private
+
+    def find_comment
+        @comment = Comment.find(params[:id])
+    end
+
+    def serialized_comment
+        @comment.to_json(include: :event)
+    end
+
+    def comment_params
+        params.require(:comment).permit(:content, :event_id)
+    end
 end
--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -7,12 +7,44 @@ class Api::EventController < ApplicationController
     render json: Event.all
   end
 
+  # def show
+  
+  # end
+
   def create
-    event = @current_user.events.create!(event_params)
-    render json: event, status: :created
+    @event = current_user.events.create!(event_params)
+    render json: @event, status: :created
   end
 
+  #patch "/events/:id"
+  def update
+    if current_user.posts.include?(@event)
+      @event&.update!(event_params)
+      render json: @event
+    else
+      render_unprocessable_entity_response
+    end
+  end
+
+  #delete "/events/:id"
+  def destroy
+    if current_user.posts.include?(@event)
+      if @event&.destroy
+        render json: { message: "Successfully destroyed event"}
+      else
+        render json: { error: @event.errors.full_messages.to_sentence }
+      end
+    else
+      render_unprocessable_entity_response
+    end
+  end
+
+
   private
+
+  def find_event
+    @event = Event.find(params[:id])
+  end
 
   def event_params
     params.permit(:name, :image_url, :date, :start_time, :venue)
