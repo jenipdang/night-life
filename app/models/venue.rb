c4e5625af2d9b31f887b3aeaf5de002534abc91f
Jeni Dang <jenipdang@yahoo.com>
2022-05-17 19:09:23 -0700

    add validations to venue model

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,7 @@
 class Venue < ApplicationRecord
+    has_many :events
+
+    validates :name, :address, :city, :state, :zip_code, presence: true
+    validates :zip_code, format: {with: /^\d{5}(-\d{4})?$/}, message: "should be 12345 or 12345-1234"
     
 end
